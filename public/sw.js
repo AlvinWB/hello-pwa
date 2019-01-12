b4eaea9ce564bd5b1c1bfabb2b3246f6962b51c7
@irwanphan <irwanphan@gmail.com>
2019-01-12 09:19:37 +0700

    sw

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+// const staticCacheName = 'hello-cache-v0.1';
+// const filesToCache = [
+//     '.',
+//     'index.html'
+// ];
+
+// self.addEventListener('install', event => {
+//     console.log('Ready to cache');
+//     event.waitUntil(
+//         caches.open(staticCacheName)
+//         .then(cache => {
+//             return cache.addAll(filesToCache);
+//         })
+//     );
+// });
+
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.filter(function(cacheName) {
+//           return cacheName.startsWith('hello-') &&
+//                  cacheName != staticCacheName;
+//         }).map(function(cacheName) {
+//           return caches.delete(cacheName);
+//         })
+//       );
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//         caches.match(event.request)
+//         .then( ada_response => {
+//             if (ada_response) {
+//                 return ada_response;
+//             }
+//             // tidak ada response, ambil ke jaringan
+//             else {
+//                 return fetch(event.request)
+//             }
+//         })
+//         .catch(error => {
+//             return new Response("Damn it " + error);
+//         })
+//     );
+// });
